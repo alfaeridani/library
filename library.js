eadc8f03ab528bd12f95eb9e6a3c7bca6e9a28f1
alfaeridani <115158881+alfaeridani@users.noreply.github.com>
2024-06-07 15:53:21 +0700

    Create basic script for adding books to the library

---
+++ library.js
@@ -0,0 +1,48 @@
+function Book([name, author, pages, read]) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    let name = prompt("What is the book title?", "The Woods");
+    let author = prompt("Who is the author?", "Alfa Eridani");
+    let pages = prompt("How many pages?", "246");
+    let read = prompt("Have you read this book?", "No");
+    newBook = new Book([name, author, pages, read]);
+    myLibrary.push(newBook);
+    return;
+}
+
+function displayBooks() {
+    let bookContainer = document.querySelector(".bookContainer");
+    
+    for(let i = 0; i < myLibrary.length; i++) {
+        let singleBookContainer = document.createElement("div");
+        bookContainer.appendChild(singleBookContainer);
+
+        let displayName = document.createElement("p");
+        displayName.textContent = myLibrary[i].name;
+            
+        let displayAuthor = document.createElement("p");
+        displayAuthor.textContent = myLibrary[i].author;
+
+        let displayPages = document.createElement("p");
+        displayPages.textContent = myLibrary[i].pages;
+
+        let displayRead = document.createElement("p");
+        displayRead.textContent = myLibrary[i].read;
+
+        singleBookContainer.appendChild(displayName);
+        singleBookContainer.appendChild(displayAuthor);
+        singleBookContainer.appendChild(displayPages);
+        singleBookContainer.appendChild(displayRead);
+    }
+    return;
+}
+
+const myLibrary = [];
+
+let addBookButton = document.querySelector("#addBook");
+addBookButton.addEventListener("click", addBookToLibrary);
